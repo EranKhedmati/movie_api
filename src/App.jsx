73bacae3b5eae3a566433d3e9fd07b3402b239c2
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-04 21:09:47 +0330

    inatll font awesome for icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 function App() {
 
   return (
     <>
       <div className="text-red-500">
         Hello
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
       </div>
     </>
   )

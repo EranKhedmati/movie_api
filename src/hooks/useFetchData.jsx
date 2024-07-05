0323b309ade7cdaa8153eb58c48e9af19aedbe19
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-05 18:00:19 +0330

    update to async version

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
 
 export function useFetchData(url) {
-    const [movieList, setMovieList] = useState([]);
-    const [metaData, setMetaData] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [metaData, setMetaData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        fetch(url)
-            .then(Response => Response.json())
-            .then(data => {
-                setMovieList(data.data);
-                setMetaData(data.metadata);
-            })
-            .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setMovieList(data.data);
+        setMetaData(data.metadata);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return [movieList, metaData];
+  return [movieList, metaData, isLoading];
 }

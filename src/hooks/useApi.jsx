79411ca458b7640291941b58e918ccadd127009d
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-07-06 17:45:16 +0330

    update fetch data custom hook

---
+++ src/hooks/useApi.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useGetApi(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const Response = await axios.get(url);
+        // console.log(Response.data);
+        // console.log(Response.status);
+        // console.log(Response.config);
+        setData(Response.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getData()
+  }, []);
+
+  return {data, loading};
+}
+
+export {useGetApi}
